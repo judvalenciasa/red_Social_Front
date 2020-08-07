f4ff4fb706236ed9a7193a134e0dfa2740032861
Sebas Giraldo <juan.1701622826@ucaldas.edu.co>
2020-08-07 18:54:44 -0500

    Crear Usuario con CSS y HTML ok

--- src/app/modulos/parametros/usuario/crear-usuario/crear-usuario.component.ts
+++ src/app/modulos/parametros/usuario/crear-usuario/crear-usuario.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { UsuariosModel } from '../../../../modelos/usuarios.model';
+import { UsuariosService } from '../../../../servicios/usuarios.service';
+import { ActivatedRoute, Router } from '@angular/router';
+
+declare const ShowNotificationMessage: any;
 
 @Component({
   selector: 'app-crear-usuario',
@@ -7,9 +13,84 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CrearUsuarioComponent implements OnInit {
 
-  constructor() { }
+  fgValidator: FormGroup;
 
-  ngOnInit(): void {
+  constructor(
+    private fb: FormBuilder,
+    private servicio: UsuariosService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) {}
+
+   /*Se ejecuta al inicio del componente*/
+   ngOnInit(): void {
+    this.crearFormulario();
   }
 
-}
+  crearFormulario()
+  {
+    this.fgValidator = this.fb.group({
+
+      primer_Nombre: ['',[Validators.required, Validators.minLength(2)]],
+      segundo_Nombre: [],
+      primer_Apellido: ['',[Validators.required, Validators.minLength(2)]],
+      segundo_Apellido: [],
+      cedula: ['',[Validators.required, Validators.minLength(7), Validators.maxLength(11)]],
+      celular: ['',[Validators.required, Validators.maxLength(10)]],
+      correo: ['',[Validators.required, Validators.email]],
+      fecha_Nacimiento: ['',[Validators.required]],
+      foto_Cedula: [],
+      foto_Personal: [],
+      rol: ['',[Validators.required]]
+
+    });
+  }
+
+  registrarUsuario()
+  {
+    if (this.fgValidator.invalid) 
+    {
+      ShowNotificationMessage('Formulario invalido');
+    }
+    else 
+    {
+      //alert(JSON.stringify(this.fgValidator.value, null, 4));
+      //alert('valido');
+      let modelo = this.getUsuariosData();
+      console.log(modelo);
+      this.servicio.registrarUsuario(modelo).subscribe(data => {
+        console.log(data);
+        if (data) {
+          ShowNotificationMessage('registro exitoso');
+          this.router.navigate(['parametros/usuario']);
+        }
+        else {
+          ShowNotificationMessage('Error en el registro');
+        }
+      });
+    }
+  }
+
+  /* Crear una instancia del modelo para ser enviada */
+  getUsuariosData(): UsuariosModel
+  {
+    let modelo = new UsuariosModel();
+    modelo.primer_Nombre = this.fgv.primer_Nombre.value;
+    modelo.segundo_Nombre = this.fgv.segundo_Nombre.value;
+    modelo.primer_Apellido = this.fgv.primer_Apellido.value;
+    modelo.segundo_Apellido = this.fgv.segundo_Apellido.value;
+    modelo.cedula = this.fgv.cedula.value;
+    modelo.celular = this.fgv.celular.value;
+    modelo.correo = this.fgv.correo.value;
+    modelo.fecha_Nacimiento = this.fgv.fecha_Nacimiento.value;
+    modelo.foto_Cedula = this.fgv.foto_Cedula.value;
+    modelo.foto_Personal = this.fgv.foto_Personal.value;
+    modelo.rol = this.fgv.rol.value;
+    return modelo;
+  }
+
+  get fgv()
+  {
+    return this.fgValidator.controls;
+  }
+} 
