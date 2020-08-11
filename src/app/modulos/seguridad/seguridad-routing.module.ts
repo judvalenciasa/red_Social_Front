e5348ed03177b30db815caba9489b7fb35087ea5
judvalenciasa <56238143+judvalenciasa@users.noreply.github.com>
2020-08-11 10:26:48 -0500

    desde cero con los guardianes

--- src/app/modulos/seguridad/seguridad-routing.module.ts
+++ src/app/modulos/seguridad/seguridad-routing.module.ts
@@ -1,34 +1,31 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { IniciarComponent} from './iniciar/iniciar.component';
+import { IniciarComponent } from './iniciar/iniciar.component';
 import { CerrarComponent } from './cerrar/cerrar.component';
-import { RecuperarClaveComponent} from './recuperar-clave/recuperar-clave.component';
+import { RecuperarClaveComponent } from './recuperar-clave/recuperar-clave.component';
 import { CambiarClaveComponent } from './cambiar-clave/cambiar-clave.component';
-import { AutenticionUsuarioGuard } from '../../guardianes/autenticion-usuario.guard';
 
 const routes: Routes = [
   {
-    path:'iniciar',
-    component:IniciarComponent,
-    canActivate: [AutenticionUsuarioGuard]
+    path: 'iniciar',
+    component: IniciarComponent,
   },
   {
-    path:'cerrar',
-    component:CerrarComponent
+    path: 'cerrar',
+    component: CerrarComponent,
   },
   {
-    path:'recuperar-clave',
-    component:RecuperarClaveComponent
+    path: 'recuperar-clave',
+    component: RecuperarClaveComponent,
   },
   {
-    path:'cambiar-clave',
-    component:CambiarClaveComponent
-  }
-
+    path: 'cambiar-clave',
+    component: CambiarClaveComponent,
+  },
 ];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class SeguridadRoutingModule { }
+export class SeguridadRoutingModule {}
