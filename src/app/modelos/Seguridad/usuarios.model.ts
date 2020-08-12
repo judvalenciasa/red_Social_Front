68ed1ed9ad4ae9a83655b130fb0d6cc91daab3e9
Sebas Giraldo <juan.1701622826@ucaldas.edu.co>
2020-08-12 12:13:54 -0500

    Actualizacion

---
+++ src/app/modelos/Seguridad/usuarios.model.ts
@@ -0,0 +1,16 @@
+export class UsuariosModel
+{
+    id?: String;
+    primer_Nombre: String;
+    segundo_Nombre: String;
+    primer_Apellido: String;
+    segundo_Apellido: String;
+    cedula: String;
+    celular: String;
+    correo: String;
+    fecha_Nacimiento: String;
+    foto_Cedula: String;
+    foto_Personal: String;
+    clave: String;
+    rol: String;
+}
